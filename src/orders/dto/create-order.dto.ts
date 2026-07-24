96c690374612f6570adde498471ec290aafae6b8
Mahsinusa <mdmahsin426@gmail.com>
2026-07-24 05:32:11 -0400

    Add files via upload

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,39 @@
+import { IsArray, IsEnum, IsOptional, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ZoneFilter } from '../../products/dto/query-products.dto';
 
+class OrderItemDto {
+  @IsString() productId: string;
+  quantity: number;
+}
+
+export enum PaymentMethodDto {
+  cod = 'cod',
+  bkash = 'bkash',
+  nagad = 'nagad',
+  sslcommerz = 'sslcommerz',
+}
+
+export class CreateOrderDto {
+  @IsEnum(ZoneFilter)
+  zone: ZoneFilter;
+
+  @IsString()
+  addressId: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+
+  @IsOptional()
+  @IsString()
+  couponCode?: string;
+
+  @IsEnum(PaymentMethodDto)
+  paymentMethod: PaymentMethodDto;
+
+  @IsOptional()
+  @IsString()
+  customBazarNote?: string;
+}
